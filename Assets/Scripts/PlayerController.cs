9e4628e5920e2e7dca671621126150f8f638b22d
nikoGUS05 <nicholas.gusniowski@connect.7oaks.org>
2020-08-14 15:06:36 -0500

    Jumping Animation Fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,20 +8,36 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
 
     public int jumpForce;
+
+    public Transform groundPoint;
+
+    public LayerMask groundLayer;
+
+    public bool grounded;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    //Update method used for physics in our game, because it happens every fixed amount of time
 
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapPoint(groundPoint.position, groundLayer);
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+            
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
         {
             anim.SetTrigger("Jump");
             rb.AddForce(Vector2.up * jumpForce);
         }
+
+       anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("Grounded", grounded);
 
     }
 }
